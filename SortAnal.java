b368ef5d71071747e404690c03a2f071fd8a91db
opee2012 <jborth2010@gmail.com>
2022-09-27 10:41:49 -0400

    added ascending and descending driver

--- SortAnal.java
+++ SortAnal.java
@@ -8,18 +8,24 @@ Use the suggested SortAnal name, if convenient. (Future programming assignments 
 import java.util.Random;
 
 public class SortAnal {
-    public static void main(String[] a){
-        int SIZE;
+    public static void Drivers(int run){
+        int array_size;
         Random rd = new Random();
 
         for (int i = 1; i <= 20; i++) {
-            SIZE = i * 1000;
-            int[] sortArray = new int[SIZE];
+            array_size = i * 1000;
+            int[] sortArray = new int[array_size];
 
-            for (int j = 0; j < SIZE; j++) {
-                sortArray[j] = rd.nextInt(1000000);
+            for (int j = 0; j < array_size; j++) {
+                if (run == 1) {
+                    sortArray[j] = j;
+                } else if (run == 2) {
+                    sortArray[j] = (j * -1);
+                } else {
+                    sortArray[j] = rd.nextInt(1000000);
+                }
             }
-            System.out.println(SIZE + ":\t" + SortAnalysis(sortArray));
+            System.out.println(array_size + ":\t" + SortAnalysis(sortArray));
         }
     }
 
@@ -42,5 +48,14 @@ public class SortAnal {
             A[j + 1] = v;
         }
         return count;
+    }
+
+    public static void main(String[] a){
+        int sortType = 0;
+        Drivers(sortType);
+        sortType = 1;
+        Drivers(sortType);
+        sortType = 2;
+        Drivers(sortType);
     }
 }
