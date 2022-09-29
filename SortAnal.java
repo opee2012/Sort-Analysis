13db191fa86a0481e98657188de30057bd95bbfc
opee2012 <jborth2010@gmail.com>
2022-09-29 10:57:21 -0400

    continued work

--- SortAnal.java
+++ SortAnal.java
@@ -5,27 +5,50 @@ Use the suggested SortAnal name, if convenient. (Future programming assignments 
 */
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 public class SortAnal {
-    public static void Drivers(int run){
+    public static void Print(int _count, int sortType, int arraySize){
+        try {
+            FileWriter sheets = new FileWriter("sheets.txt", true);
+            while (sortType <= 2){
+                if (sortType == 0) {
+                    sheets.write(arraySize + "\t");
+                    sheets.append(_count + "\r\n");
+                    Drivers(sortType);
+                }
+            }
+            sheets.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void Drivers(int _run){
         int array_size;
         Random rd = new Random();
 
         for (int i = 1; i <= 20; i++) {
+            if (_run == 3) break;
             array_size = i * 1000;
             int[] sortArray = new int[array_size];
 
             for (int j = 0; j < array_size; j++) {
-                if (run == 1) {
+                if (_run == 1) {
                     sortArray[j] = j;
-                } else if (run == 2) {
+                } else if (_run == 2) {
                     sortArray[j] = (j * -1);
                 } else {
                     sortArray[j] = rd.nextInt(1000000);
                 }
             }
-            System.out.println(array_size + ":\t" + SortAnalysis(sortArray));
+            Print(SortAnalysis(sortArray), _run, i);
+            if (i == 20) {
+                _run++;
+                i = 0;
+            }
         }
     }
 
@@ -51,11 +74,19 @@ public class SortAnal {
     }
 
     public static void main(String[] a){
-        int sortType = 0;
-        Drivers(sortType);
-        sortType = 1;
-        Drivers(sortType);
-        sortType = 2;
-        Drivers(sortType);
+        int run = 0;
+
+        try {
+            File sheets = new File("sheets.txt");
+            if (sheets.createNewFile()) {
+                System.out.println("File created: " + sheets.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        Drivers(run);
     }
 }
